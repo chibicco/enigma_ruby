2dc3d1b4c5e4bbac7deeb7cae4fc61f4321b96b9
chibicco <me@chibicco.dev>
2024-07-15 20:59:11 +0900

    refactor: Update Plugboard class to use an array of pairs for swapping

--- lib/enigma_ruby/plugboard.rb
+++ lib/enigma_ruby/plugboard.rb
@@ -1,14 +1,22 @@
 
 module EnigmaRuby
   class Plugboard
-    WIRING = {
-      'A' => 'F', 'F' => 'A',
-      'B' => 'E', 'E' => 'B',
-      'C' => 'D', 'D' => 'C'
-    }.freeze
+    PAIRS = %w[AF BE CD].freeze
 
     def self.swap(char)
-      WIRING[char] || char
+      check_duplicates
+
+      PAIRS.each do |pair|
+        return pair[1] if pair[0] == char
+        return pair[0] if pair[1] == char
+      end
+      char
+    end
+
+    def self.check_duplicates
+      characters = PAIRS.join.chars
+      duplicates = characters.select { |char| characters.count(char) > 1 }.uniq
+      raise "Duplicate characters found: #{duplicates.join(', ')}" if duplicates.any?
     end
   end
 end
