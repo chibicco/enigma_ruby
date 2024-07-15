f3538e12ebd4900ec04839b9792e3c2c9a85b771
chibicco <me@chibicco.dev>
2024-07-15 20:43:56 +0900

    chore: Update Rotor class to use dynamic wiring size

--- lib/enigma_ruby/rotor.rb
+++ lib/enigma_ruby/rotor.rb
@@ -6,21 +6,27 @@ module EnigmaRuby
     end
 
     def encode_forward(char)
-      index = (char.ord - 'A'.ord + @position) % 26
+      index = (char.ord - 'A'.ord + @position) % wiring_size
       @wiring[index]
     end
 
     def encode_backward(char)
       index = @wiring.index(char)
-      ((index - @position) % 26 + 'A'.ord).chr
+      ((index - @position) % wiring_size + 'A'.ord).chr
     end
 
     def advance
-      @position = (@position + 1) % 26
+      @position = (@position + 1) % wiring_size
     end
 
     def at_notch?
       @position == @notch
+    end
+
+    private
+
+    def wiring_size
+      @wiring.size
     end
   end
 end
